3a8eb7268db3504758ec590dba8a94af073ed49b
pl9shy <neo.wwh1@gmail.com>
2024-07-13 12:47:04 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine($"Введите сумму в долларах: ");
+double dollars = Convert.ToDouble(Console.ReadLine());
+
+double Rate = 87.99;
+double rubles = dollars * Rate;
+
+Console.WriteLine($"{rubles.ToString("C", new System.Globalization.CultureInfo("ru-ru"))} = {dollars.ToString("C", 
+new System.Globalization.CultureInfo("en-US"))}");
+
+Console.ReadLine();
