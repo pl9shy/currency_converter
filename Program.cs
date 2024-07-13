88756c583139a73e3e7bf27420bba28333edd26e
pl9shy <neo.wwh1@gmail.com>
2024-07-13 12:48:18 +0500

    Added ability to choose between currencies

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿Console.WriteLine($"Введите сумму в долларах: ");
-double dollars = Convert.ToDouble(Console.ReadLine());
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
 
-double Rate = 87.99;
-double rubles = dollars * Rate;
+Console.WriteLine("Выберите тип конвертации:\n1 - Рубли -> Доллары\t 2 - Доллары -> Рубли");
+string type = Console.ReadLine();
 
-Console.WriteLine($"{rubles.ToString("C", new System.Globalization.CultureInfo("ru-ru"))} = {dollars.ToString("C", 
-new System.Globalization.CultureInfo("en-US"))}");
+double rate = 87.74;
+
+switch (type)
+{
+    case "1":
+        Console.WriteLine($"Введите сумму в рублях: ");
+        
+        double rublesQuantity = Convert.ToDouble(Console.ReadLine());
+        double quantityDollars = rublesQuantity / rate;
+
+        Console.WriteLine($"{quantityDollars.ToString("C", new System.Globalization.CultureInfo("en-US"))} = {rublesQuantity.ToString("C", 
+        new System.Globalization.CultureInfo("ru-ru"))}");
+
+        break;
+
+    case "2":
+        Console.WriteLine($"Введите сумму в долларах: ");
+
+        double dollarsQuantity = Convert.ToDouble(Console.ReadLine());
+        double quantityRubles = dollarsQuantity * rate;
+
+        Console.WriteLine($"{quantityRubles.ToString("C", new System.Globalization.CultureInfo("ru-ru"))} = {dollarsQuantity.ToString("C", 
+        new System.Globalization.CultureInfo("en-US"))}");
+
+        break;
+}
 
 Console.ReadLine();
